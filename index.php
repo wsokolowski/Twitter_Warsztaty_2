d74874a95d8016140d3db4ca640939fece5428c0
wsokolowski <w.sokolowski111@gmail.com>
2016-07-01 12:25:27 +0200

    Version 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
+
+password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+
+
+
